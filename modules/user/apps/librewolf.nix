9cd7986c1e20b22197656154b597b679e9ff03e1
vebly <>
2024-06-25 14:21:00 +0200

    Added librewolf configuration

---
+++ modules/user/apps/librewolf.nix
@@ -0,0 +1,15 @@
+{pkgs, ...}:
+{
+    programs.librewolf ={
+        enable = true;
+        package = pkgs.librewolf;
+        settings = {
+            "privacy.clearOnShutdown.history" = false;
+            "privacy.clearOnShutdown.downloads" = false;
+            "privacy.clearOnShutdown.cookies" = false;
+            "network.cookie.lifetimePolicy" = 0;
+            "browser.newtabpage.activity-stream.feeds.topsites" = true;
+            "browser.newtabpage.activity-stream.default.sites" = "https://www.youtube.com/,https://chatgpt.com,https://search.nixos.org/packages,https://home-manager-options.extranix.com/";
+        };
+    };
+}
