a3f9bee93e54942684790bb2f60becc913005582
vebly <>
2024-09-11 20:23:50 +0200

    added dracula colors to zsh

--- modules/user/sh/zsh/zsh.nix
+++ modules/user/sh/zsh/zsh.nix
@@ -1,4 +1,18 @@
 { pkgs, lib, var, ... }:
+let 
+    dracula_green = "#8aff80";
+    dracula_yellow = "#F1FA8C";
+    dracula_blue = "#8BE9FD";
+    dracula_orange = "#FFB86C";
+    dracula_purple = "#BD93F9";
+    dracula_red = "#FF5555";
+
+    command_color = "fg=${dracula_green}";
+    error_color = "fg=${dracula_red}";
+    reserved_color = "fg=${dracula_yellow}"; 
+    globbing_color = "fg=${dracula_blue}"; 
+    precommand_color = "fg=${dracula_orange}";
+in
 {
     home.packages = with pkgs; [
         zsh
@@ -8,7 +22,23 @@
             enable = true;
             enableCompletion = true;
             autosuggestion.enable = true;
-            syntaxHighlighting.enable = true;
+            syntaxHighlighting = {
+                enable = true;
+                styles = {
+                    # Options: https://github.com/zsh-users/zsh-syntax-highlighting/blob/master/docs/highlighters/main.md
+                    builtin = command_color;
+                    command = command_color;
+                    alias = command_color;
+                    suffix-alias = command_color;
+                    global-alias = command_color;
+                    precommand = precommand_color; 
+                    paths = command_color;
+                    function = command_color;
+                    unknown-token = error_color;  
+                    reserved-word = reserved_color; 
+                    globbing = globbing_color;
+                };
+            };
             prezto.tmux.autoStartLocal = true;
             shellAliases = {
                 "cpwd" = "pwd | tr -d \"\n\" | xargs echo -n | xclip -selection clipboard";
@@ -21,7 +51,7 @@
             initExtra = ''
                 # eval "$(tmuxifier init -)"
                 eval "$(zoxide init --cmd cd zsh)" #ZOXIDE alias
-                PS1="%F{#00ffff}%n@ %~ %F{#00ffff}%f "
+                PS1="%F{${dracula_purple}}%n@ %~ %F{${dracula_purple}}%f "
 
                 up() {
                     if [[ $# -ne 1 ]] || ! [[ $1 =~ ^[0-9]+$ ]]; then
