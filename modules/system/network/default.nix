66e88017881764c94c63163f039b71088916ace1
vebly <>
2024-09-14 10:14:28 +0200

    smb now shows up in nemo

--- modules/system/network/default.nix
+++ modules/system/network/default.nix
@@ -1,7 +1,11 @@
-{config, pkgs, ...}:
+{config, pkgs, lib, ...}:
 {
+    environment.systemPackages = with pkgs; [lxqt.lxqt-policykit]; #Needed by gvfs (Usually comes with desktop environments)
     networking.networkmanager.enable = true;
-    services.gvfs.enable = true; # Creates virtual file system necessary to access network stuff like samba shares. 
+    services.gvfs = {
+        enable = true; # Creates virtual file system necessary to access network stuff like samba shares. 
+        package = lib.mkForce pkgs.gnome3.gvfs;
+    };
     services.avahi = { #Allows file manager to find shares
         enable = true;
         nssmdns4 = true; 
