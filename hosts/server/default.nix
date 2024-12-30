08478f07af0b8f860e37aa71c5a34f232ee1ef63
vebly <>
2024-12-30 17:48:34 +0100

    fix share

--- hosts/server/default.nix
+++ hosts/server/default.nix
@@ -33,6 +33,7 @@
         enable = true;
         allowedTCPPorts = [
             8384 #Syncthing gui
+            631 # CUPS gui
         ]; 
     };
 
@@ -59,51 +60,53 @@
     services.samba = {
        enable = true;
        package = pkgs.sambaFull;
-       enableNmbd = true;
-       enableWinbindd = true;
-       securityType = "user";
+       nmbd.enable = true;
+       winbindd.enable = true;
        openFirewall = true;
        nsswins = true;
-       extraConfig = ''
-       hosts allow = 192.168.0.0/16
-       hosts deny = 0.0.0.0/0
-       netbios name = NAS
+       settings = {
+           global = {
+               "hosts allow" = "192.168.0.0/16";
+               "netbios name" = "NAS";
+               security = "user";
+           };
+           Vebly = {
+           "path" = "/mnt/sdb/private/vebly";
+           "valid users" = "vebly";
+           "force user" = "smbuser";
+           "force group" = "smbgroup";
+           "create mask" = "0664";
+           "force create mode" = "0664";
+           "directory mask" = "0775";
+           "force directory mode" = "0775";
+           "writable" = "yes";
+           };
 
+           Media = {
+               "path" = "/mnt/sdb/public/media";
+               "valid users" = "klee vebly";
+               "force user" = "smbuser";
+               "force group" = "smbgroup";
+               "force create mode" = "0664";
+               "directory mask" = "0775";
+               "force directory mode" = "0775";
+               "browsable" = "yes";
+               "read only" = "no";
+               "writable" = "yes";
+           };
 
-       [Media]
-       path = /mnt/sdb/public/media
-       valid users = klee vebly
-       force user = smbuser
-       force group = smbgroup
-       force create mode = 0664
-       directory mask = 0775
-       force directory mode = 0775
-       browsable = yes
-       read only = no
-       writable = yes
-
-       [Honigklee]
-       path = /mnt/sdb/private/honigklee
-       valid users = klee vebly
-       force user = smbuser
-       force group = smbgroup
-       create mask = 0664
-       force create mode = 0664
-       directory mask = 0775
-       force directory mode = 0775
-       writable = yes
-
-       [Vebly]
-       path = /mnt/sdb/private/vebly
-       valid users = vebly
-       force user = smbuser
-       force group = smbgroup
-       create mask = 0664
-       force create mode = 0664
-       directory mask = 0775
-       force directory mode = 0775
-       writable = yes
-       '';
+           Honigklee = {
+               "path" = "/mnt/sdb/private/honigklee";
+               "valid users" = "klee vebly";
+               "force user" = "smbuser";
+               "force group" = "smbgroup";
+               "create mask" = "0664";
+               "force create mode" = "0664";
+               "directory mask" = "0775";
+               "force directory mode" = "0775";
+               "writable" = "yes";
+           };
+       };
     };
 
     services.samba-wsdd = {
@@ -116,6 +119,15 @@
     networking.wlanInterfaces."wlan-monitor" = {
         device = "wlp0s20u3u2";
         type = "monitor";
+    };
+
+    services.printing = {
+        enable = true;
+        # defaultShared = true;
+        # browsing = true;
+        # drivers = with pkgs;[canon-cups-ufr2 canon-capt];
+        # listenAddresses = [ "0.0.0.0:631"];
+        # allowFrom = ["all"];
     };
 
     environment.systemPackages =  with pkgs; [
@@ -137,4 +149,5 @@
     nix.settings.experimental-features = ["nix-command" "flakes"]; 
 
 }
+
 
