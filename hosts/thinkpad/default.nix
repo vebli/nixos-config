4cea8d553c58fe75e77ae6d0db41aeced0bc5ece
vebly <>
2026-04-16 11:10:57 +0200

    fixed throttling issue

--- hosts/thinkpad/default.nix
+++ hosts/thinkpad/default.nix
@@ -33,6 +33,23 @@
     };
   };
 
+  services.power-profiles-daemon.enable = false;
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "powersave";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "performance";
+      # CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_performance";
+
+      CPU_MIN_PERF_ON_AC = 0;
+      CPU_MAX_PERF_ON_AC = 100;
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_MAX_PERF_ON_BAT = 100;
+    };
+  };
   # programs.nix-ld = {
   #   enable = true;
   #   libraries= with pkgs; [
